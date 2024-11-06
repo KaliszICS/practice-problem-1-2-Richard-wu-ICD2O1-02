c2a50d5efcbe10eb0f1260a3e368e641b3007d33
Richard-wu-ICD2O1-02 <Richard.wu4@student.tdsb.on.ca>
2024-11-05 19:04:52 -0500

    Update PP1_2.py

--- PP1_2.py
+++ PP1_2.py
@@ -1,27 +1,53 @@
-
-
+'''
+    Lesson: Variables and Data Types
+    Author: Richard wu
+    Date Created: Nov 1, 2024
+    Date Last Modified: Nov 4rd, 2024
+'''
 
 
 def q1():
   #Write Assignment code here
+    name = "Alex"
+    print(name)
 
 def q2():
   #Write Assignment code here
+    four = 4
+    five = 5
+    three = 3
+    print(four + five + three)
 
 def q3():
   #Write Assignment code here
+    twoOne = 21
+    six = 6
+    print(twoOne//six)
 
 def q4():
   #Write Assignment code here
+   sixPointThree = 6.3
+    threePointO = 3.0
+    print(sixPointThree/threePointO)
 
 def q5():
   #Write Assignment code here
+ hiworld = "Hello World"
+    print(hiworld)
+    hiworld = "Goodbye World"
+    print(hiworld)
 
 def q6():
   #Write Assignment code here
+    real =  True
+    print(real)
 
 def q7():
   #Write Assignment code here
+    eight = 8
+    three = 3
+    print(eight%three)
+    
 
   
 #DO NOT EDIT ANYTHING AFTER THIS COMMENT
